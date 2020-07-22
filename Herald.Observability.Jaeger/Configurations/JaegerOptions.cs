92c6870fbbaa232e28b4ff2972df0408686ad300
tcfialho <tcfialho@gmail.com>
2020-07-22 03:40:01 -0300

    feat: jaeger configurations

---
+++ Herald.Observability.Jaeger/Configurations/JaegerOptions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.PlatformAbstractions;
+
+namespace Herald.Observability.Jaeger.Configurations
+{
+    /// <summary>
+    ///     Config for the Jaeger Client, see more https://github.com/jaegertracing/jaeger-client-csharp
+    /// </summary>
+    public class JaegerOptions
+    {
+        /// <summary>
+        /// The service name.
+        /// </summary>
+        public string ServiceName { get; set; } = PlatformServices.Default.Application.ApplicationName;
+
+        /// <summary>
+        /// The hostname for communicating with agent via UDP.
+        /// </summary>
+        public int AgentPort { get; set; } = 6831;
+
+        /// <summary>
+        /// The port for communicating with agent via UDP.
+        /// </summary>
+        public string AgentHost { get; set; } = "localhost";
+
+        /// <summary>
+        /// The url for the remote sampling conf when using sampler type remote.
+        /// </summary>
+        public string SamplingEndpoint { get; set; } = $"http://localhost:5778";
+
+        /// <summary>
+        /// The traces endpoint, in case the client should connect directly to the collector.
+        /// </summary>
+        public string Endpoint { get; set; } = "http://localhost:14268/api/traces";
+
+        /// <summary>
+        /// Username to send as part of "Basic" authentication to the endpoint.
+        /// </summary>
+        public string User { get; set; }
+
+        /// <summary>
+        /// Password to send as part of "Basic" authentication to the endpoint.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Authentication Token to send as "Bearer" to the endpoint.
+        /// </summary>
+        public string AuthToken { get; set; }
+
+        public JaegerOptions()
+        {
+
+        }
+    }
+}
---
+++ Herald.Observability.Jaeger/Configurations/ServiceCollectionExtensions.cs
@@ -0,0 +1,115 @@
+﻿using Jaeger;
+using Jaeger.Samplers;
+using Jaeger.Senders;
+using Jaeger.Senders.Thrift;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using OpenTracing.Contrib.NetCore.CoreFx;
+using OpenTracing.Util;
+
+using System;
+
+namespace Herald.Observability.Jaeger.Configurations
+{
+    public static partial class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJaegerTracing(this IServiceCollection services)
+        {
+            var jaegerOptions = JaegerOptionsFactory.Create();
+
+            return services.AddJaegerTracing(jaegerOptions);
+        }
+
+        public static IServiceCollection AddJaegerTracing(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jaegerOptions = JaegerOptionsFactory.Create(configuration);
+
+            return services.AddJaegerTracing(jaegerOptions);
+        }
+
+        public static IServiceCollection AddJaegerTracing(this IServiceCollection services, JaegerOptions options)
+        {
+            return services.AddJaegerTracing(configure =>
+            {
+                options.CopyTo(configure);
+            });
+        }
+
+        public static IServiceCollection AddJaegerTracing(this IServiceCollection services, Action<JaegerOptions> configure)
+        {
+            services.Configure(configure);
+
+            var options = new JaegerOptions();
+
+            configure?.Invoke(options);
+            services.AddOpenTracing();
+
+            // Add ITracer
+            services.AddSingleton(serviceProvider =>
+            {
+                var configuration = serviceProvider.GetService<IConfiguration>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+
+                Configuration.SenderConfiguration.DefaultSenderResolver =
+                    new SenderResolver(loggerFactory).RegisterSenderFactory<ThriftSenderFactory>();
+
+                var tracerConfig = GetTracerConfig(options, loggerFactory);
+
+                // Tracer
+                var tracer = tracerConfig.GetTracer();
+
+                if (!GlobalTracer.IsRegistered())
+                {
+                    GlobalTracer.Register(tracer);
+                }
+
+                return tracer;
+            });
+
+            services.Configure<HttpHandlerDiagnosticOptions>(o =>
+            {
+                o.IgnorePatterns.Add(x => !x.RequestUri.IsLoopback);
+            });
+
+            return services;
+        }
+
+        private static Configuration GetTracerConfig(JaegerOptions options, ILoggerFactory loggerFactory)
+        {
+            // Sender
+            var senderConfig = new Configuration.SenderConfiguration(loggerFactory);
+            senderConfig.WithAgentHost(options.AgentHost)
+                        .WithAgentPort(options.AgentPort)
+                        .WithEndpoint(options.Endpoint);
+
+            if (!string.IsNullOrEmpty(options.User))
+            {
+                senderConfig.WithAuthUsername(options.User)
+                            .WithAuthPassword(options.Password);
+            }
+            if (!string.IsNullOrEmpty(options.AuthToken))
+            {
+                senderConfig.WithAuthToken(options.AuthToken);
+            }
+
+            // Sampler
+            var samplerConfig = new Configuration.SamplerConfiguration(loggerFactory)
+                        .WithSamplingEndpoint(options.SamplingEndpoint)
+                        .WithType(ConstSampler.Type);
+
+            // Reporter
+            var reporterConfig = new Configuration.ReporterConfiguration(loggerFactory);
+            reporterConfig.WithSender(senderConfig);
+
+            // Configuration
+            var tracerConfig = new Configuration(options.ServiceName, loggerFactory)
+                    .WithSampler(samplerConfig)
+                    .WithReporter(reporterConfig);
+
+            return tracerConfig;
+        }
+    }
+}
